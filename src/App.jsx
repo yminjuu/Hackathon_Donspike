e0e9ad6dfb3e04a42fe2ac3154fdd3078fc12531
yminjuu <ymj0580@naver.com>
2024-07-16 02:28:33 +0900

    chore: Main page 구성

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import { useState } from "react";
+import { RouterProvider } from 'react-router-dom';
+import router from './Router';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <RouterProvider router={router}></RouterProvider>
+    </>
+  );
 }
 
 export default App;
---
+++ src/Router.jsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from 'react-router-dom';
+import styled from 'styled-components';
+import Login from './pages/Login';
+import Join from './pages/Join';
+import Main from './pages/Main';
+
+const router = createBrowserRouter([
+  {
+    path: '/login',
+    element: <Login></Login>,
+  },
+  {
+    path: '/join',
+    element: <Join></Join>,
+  },
+  // {
+  //   path: '/home/:id',
+  //   element: <Home></Home>,
+  // },
+  {
+    path: '/',
+    element: <Main></Main>,
+    // 추후 바로 home으로 연결하되, 로그인 안되어있으면 login page로 route
+  },
+]);
+
+export default router;
