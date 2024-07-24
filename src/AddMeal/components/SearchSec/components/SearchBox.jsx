d3e16e9cb0f9855ff55ace0386bd1b677aba9fa8
yminjuu <ymj0580@naver.com>
2024-07-24 10:01:35 +0900

    feat: 검색창 구현 완료

--- src/AddMeal/components/SearchSec/components/SearchBox.jsx
+++ src/AddMeal/components/SearchSec/components/SearchBox.jsx
@@ -2,18 +2,71 @@ import styled from 'styled-components';
 import Hamburger from '../imgs/Hamburger.svg?react';
 import FrenchFries from '../imgs/FrenchFries.svg?react';
 import SearchButton from '../imgs/SearchButton.svg?react';
+import SearchReset from '../imgs/SearchReset.svg?react';
+import { useState, useRef, useEffect } from 'react';
+import { css } from 'styled-components';
+import SearchItem from './SearchItem';
 
 const SearchBox = () => {
+  // 검색어 관리
+  const [searchText, setSearchText] = useState('');
+
+  useEffect(() => {
+    if (searchText == '' && searchState === true) {
+      onSearchFalse();
+    }
+  }, [searchText]);
+
+  // 검색 상태 관리
+  const [searchState, toggleSearchState] = useState(false);
+  const searchInput = useRef(null);
+
+  // 검색 버튼 클릭 => Wrapper이 알맞게 바뀜
   const onSearchBtnClick = () => {
-    // console.log('검색 버튼 눌림');
+    // API POST : 음식 검색
+    if (searchText != '') {
+      if (!searchState) onSearchTrue();
+      else onSearchFalse();
+    } else {
+      searchInput.current.focus();
+    }
   };
+
+  // 검색 상태 변경
+  const onSearchTrue = () => {
+    toggleSearchState(true); // reset 아이콘으로 바뀜
+  };
+
+  const onSearchFalse = () => {
+    setSearchText('');
+    toggleSearchState(false); // 돋보기 아이콘으로 바뀜
+  };
+
   return (
     <Container>
-      <Wrapper>
-        <StyledInput placeholder="예) 흰쌀밥, 생오이"></StyledInput>
-        <ButtonWrapper onClick={onSearchBtnClick}>
-          <SearchButton></SearchButton>
-        </ButtonWrapper>
+      <Wrapper searchState={searchState}>
+        <InputBoxWrapper searchState={searchState}>
+          <StyledInput
+            // 키보드 Enter 클릭시 검색 가능
+            onKeyDown={e => {
+              if (e.key === 'Enter' && searchState === false) onSearchBtnClick();
+            }}
+            onChange={e => {
+              setSearchText(e.target.value);
+            }}
+            ref={searchInput} /* focus effect를 사용하기 위해서 useRef 사용 */
+            value={searchText}
+            placeholder="예) 흰쌀밥, 생오이"
+            searchState={searchState} /* props 전달 */
+          ></StyledInput>
+          <BtnWrapper onClick={onSearchBtnClick}>
+            {searchState === true ? <SearchReset></SearchReset> : <SearchButton></SearchButton>}
+          </BtnWrapper>
+        </InputBoxWrapper>
+        {/* {searchState === true ? <StyledNoResult>일치하는 결과가 없습니다.</StyledNoResult> : <></>} */}
+        {/* searchState===true이고 API 결과가 빈 배열 => 일치하는 결과가 없습니다*/}
+        {searchState === true ? <SearchItem></SearchItem> : <></>}
+        {/* searchState===false이고 API 결과가 있음 => 알맞게 아이템을 만들어서 해당 컴포넌트를 반환 (클릭 이벤트 필요) */}
       </Wrapper>
       <TransparentWrapper>
         <StyledHamb></StyledHamb>
@@ -32,8 +85,70 @@ const Container = styled.div`
   justify-content: space-between;
 `;
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 33.75rem;
+  height: 3.375rem;
+  justify-content: center;
+  align-items: center;
+  flex-shrink: 0;
+  position: relative;
+
+  /* drop-shadow */
+  box-shadow: 2px 4px 10px 2px #e8e8e8;
+  border-radius: 0.625rem;
+  background-color: white;
+
+  z-index: 3;
+
+  ${props =>
+    props.searchState === true
+      ? // 다중 속성을 사용
+        css`
+          height: 10rem;
+        `
+      : css``};
+
+  // input 창이 focus 받으면
+  &:focus-within {
+    ${props =>
+      props.searchState === true
+        ? // 다중 속성을 사용
+          css`
+            box-shadow: none;
+          `
+        : css`
+            box-shadow: none;
+            box-shadow: 2px 2px 4px 2px #c1ccfe;
+          `};
+  }
+`;
+
+const InputBoxWrapper = styled.div`
+  width: 30rem;
+  height: 3.375rem;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+
+  border: none;
+  outline: none;
+
+  ${props =>
+    props.searchState === true
+      ? // 다중 속성을 사용
+        css`
+          border-bottom: 1px solid #a0a0a0;
+        `
+      : css``};
+`;
+
 const StyledInput = styled.input`
   width: 80%;
+  height: 3rem;
   color: #a0a0a0;
 
   /* Pretendard/Reg/20 */
@@ -56,28 +171,8 @@ const StyledInput = styled.input`
   }
 `;
 
-const ButtonWrapper = styled.div`
-  width: 1.33069rem;
-  height: 1.33069rem;
-  flex-shrink: 0;
+const BtnWrapper = styled.div`
   cursor: pointer;
-`;
-
-const Wrapper = styled.div`
-  display: flex;
-  width: 33.75rem;
-  height: 3.375rem;
-  justify-content: center;
-  align-items: center;
-  flex-shrink: 0;
-  position: relative;
-
-  /* drop-shadow */
-  box-shadow: 2px 4px 10px 2px #e8e8e8;
-  border-radius: 10px;
-  background-color: white;
-
-  z-index: 3;
 `;
 
 // 햄버거, 감튀 요소의 position: absolute을 위해 존재하는 투명한 컨테이너 (z-index 사용을 위해서)
@@ -104,4 +199,21 @@ const StyledFrench = styled(FrenchFries)`
   left: 91%; /* Adjust based on the position of the icon */
   z-index: 1;
 `;
+
+const StyledNoResult = styled.div`
+  height: 6.66rem;
+  width: 100%;
+
+  text-align: center;
+
+  color: #414141;
+
+  /* Pretendard/Reg/16 */
+  font-family: Pretendard;
+  font-size: 0.9rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 6.66rem;
+`;
+
 export default SearchBox;
