766da8db4b369df5651ed685d48893e3e59cb2f6
yminjuu <ymj0580@naver.com>
2024-07-31 11:02:29 +0900

    feat: 예상 혈당 툴팁 추가

--- src/Graphs/components/MainBloodSugarChart.jsx
+++ src/Graphs/components/MainBloodSugarChart.jsx
@@ -46,8 +46,36 @@ const data = [
     recorddate: '2024-06-24T17:00:00.123456+09:00',
     bloodsugar: 130.0,
     foodnames: ['사과', '배'],
+    expect: 130.0, // 전날 혈당값과 이어주기 위해서 존재
+  },
+  {
+    recorddate: '2024-07-31T17:00:00.123456+09:00',
+    expect: 140.0,
+    foodnames: [],
   },
 ];
+
+//
+const formatDate = dateString => {
+  const date = new Date(dateString);
+  const month = (date.getMonth() + 1).toString().padStart(2, '0'); // 월을 2자리로 맞춤
+  const day = date.getDate().toString().padStart(2, '0'); // 일을 2자리로 맞춤
+  return `${month}/${day}`;
+};
+
+const CustomizedDot = props => {
+  const { cx, cy, stroke, payload, value } = props;
+  const date = new Date();
+  const indexDate = new Date(payload.recorddate);
+
+  if (
+    date.getFullYear() === indexDate.getFullYear() &&
+    date.getMonth() === indexDate.getMonth() &&
+    date.getDate() === indexDate.getDate()
+  ) {
+    return <circle cx={cx} cy={cy} r={5} stroke={stroke} strokeWidth={1} fill="#D6DDFE" />;
+  } else return <></>;
+};
 
 const MainBloodSugarChart = () => {
   // 데이터 중 최대 혈당량을 구함
@@ -69,12 +97,27 @@ const MainBloodSugarChart = () => {
           }}
         >
           <CartesianGrid horizontal={true} vertical={false} />
-          <XAxis dataKey="name" interval={0} tick={{ fontSize: 13 }} padding={{ left: 20, right: 20 }} />
+          <XAxis
+            dataKey="recorddate"
+            interval={0}
+            tick={{ fontSize: 13 }}
+            padding={{ left: 20, right: 20 }}
+            tickFormatter={formatDate}
+          />
           <YAxis domain={['dataMin-20', 'dataMax+5']} tickCount={6} allowDecimals={false} orientation="right" />
           {/* y축 인덱스의 최대/최소값은 혈당의 실제 최대/최소값-20 */}
-          <Tooltip content={<MainBSToolTip />} />
+          <Tooltip content={<MainBSToolTip />} wrapperStyle={{ overflow: 'visible' }} />
           <ReferenceLine y={dataMax} stroke="red" strokeDasharray="3 10"></ReferenceLine>
           <ReferenceLine y={dataMin} stroke="#A0A0A0" strokeDasharray="3 10"></ReferenceLine>
+          <Line
+            type="linear"
+            dataKey="expect"
+            stroke="#D6DDFE"
+            strokeWidth={2}
+            strokeDasharray="5 5" // 점선 설정
+            dot={<CustomizedDot />}
+            activeDot={{ r: 6, fill: '#3053f9', strokeWidth: 0 }}
+          />
           <Line
             type="linear"
             dataKey="bloodsugar"
