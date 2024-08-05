04b79db90d90fea242df42ba70ffa0acd8aa080e
yminjuu <ymj0580@naver.com>
2024-08-05 20:33:57 +0900

    fix: 오류 수정

--- src/Home/components/Section1.jsx
+++ src/Home/components/Section1.jsx
@@ -36,6 +36,7 @@ const Section1 = () => {
               <MacImg2 src={Mac2}></MacImg2>
             </MacWrap>
             <MainWrap>
+              feat/#11/LoginPage
               <MainText>혈당 스파이크를 방지할 작은 시작,</MainText>
               <LogoWrap src={Logo} />
             </MainWrap>
